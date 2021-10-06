80fd56f35db7d10002057fa422c77accd9872e32
zhiqueen <zhiqueen@sina.com>
2021-10-06 16:47:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,4 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
 # 修改默认主题为argon
-sed -i s/luci-theme-bootstrap/luci-theme-argon/g feeds/luci/collections/luci/Makefile
-# 修改密码为空，自定义名称
-ZZZ=package/lean/default-settings/files/zzz-default-settings
-sed -i /CYXluq4wUazHjmCDBCqXF/d $ZZZ
-sed -i s/OpenWrt /######################## compiled in $(TZ=UTC-8 date +%Y.%m.%d) @ OpenWrt /g $ZZZ（#为自定义内容）
-# 修改主机名称
-echo “DISTRIB_DESCRIPTION=‘OpenWrt666’” >> /etc/openwrt_release
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
